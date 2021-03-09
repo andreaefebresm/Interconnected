c80d7c455c622816d202826b5fbe2833142fb2e8
Federico <federico.izzop@gmail.com>
2021-03-09 15:51:47 +0100

    horizontal scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,69 +9,81 @@ import { useEffect, useRef } from 'react';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Index() {
-  const container = useRef();
+  const panelsContainer = useRef();
   const intro = useRef();
 
   useEffect(() => {
-    console.log(container.current);
-
-    gsap.to(container.current, {
-      x: () => `${-(container.current.scrollWidth - document.documentElement.clientWidth)}px`,
+    /* Panels */
+    const panels = gsap.utils.toArray('#panels-container .panel');
+    gsap.to(panels, {
+      xPercent: -100 * (panels.length - 1),
       ease: 'none',
       scrollTrigger: {
-        trigger: intro.current,
-        start: 'bottom top',
-        invalidateOnRefresh: true,
-        // pin: true,
+        trigger: '#panels-container',
+        pin: true,
+        start: 'top top',
         scrub: 1,
-        markers: true,
-        end: () => `+=${container.current.innerWidth}`,
+        snap: {
+          snapTo: 1 / (panels.length - 1),
+          duration: { min: 0.1, max: 0.1 },
+        },
+        end: () => `+=${panelsContainer.current.offsetWidth - innerWidth}`,
       },
     });
   }, []);
 
+  const panels = [
+    'Panel 1',
+    'Panel 2',
+    'Panel 3',
+    'Panel 4',
+    'Panel 5',
+  ];
+
   return (
     <div>
-      <div id="intro" ref={intro}>Intro</div>
-      <div id="container" ref={container}>
-        <ScrollModule>
-          <div className="row h-100">
-            <div className="col-8 display-6">
-              <p>Smart objects are devices that have network connectivity and they work by collecting user data, process and analyse that data and then return it back to the user.</p>
-            </div>
-          </div>
-        </ScrollModule>
-        <ScrollModule>
-          <div className="row h-100 align-items-end">
-            <div className="col-8 display-6">
-              <p>The data they collect comes from our interaction with the objects: your voice, your face, the map of your house and even your habits.</p>
-            </div>
-          </div>
-        </ScrollModule>
-        <ScrollModule>
-          <div className="row h-100">
-            <div className="col-8 display-6">
-              <p>Data is stored in big server farms where Artificial Intelligences analyse this data and understand stuff from it. </p>
-            </div>
-          </div>
-        </ScrollModule>
-        <ScrollModule>
-          <div className="row h-100 align-items-end">
-            <div className="col-8 display-6">
-              <p>This data is not only stored by the company that produces the object (FIRST PARTY), but also from other companies that are called second and third parties. </p>
-            </div>
-          </div>
-        </ScrollModule>
-        <ScrollModule>
-          <div className="row h-100 align-items-end">
-            <div className="col-8 display-6">
-              <p>The path of the data from first parties to second and third parties is still not clear, but we do know that data has economical value.</p>
-            </div>
-          </div>
-        </ScrollModule>
+      <main id="content" className="site-content" role="main">
 
-      </div>
+        <section id="intro" className="full-screen pt-5 blue">
+          <h1>A cool title</h1>
 
+        </section>
+
+        <section id="panels">
+
+          <div id="panels-container" style={{ width: '500%' }} ref={panelsContainer}>
+
+            {
+          panels.map((text) => (
+
+            <article id="panel-1" className="panel full-screen red">
+              <div className="container">
+                <div className="row">
+                  <div className="col-6" />
+                  <div className="col-6 d-flex flex-column">
+
+                    <h2>Panel 1</h2>
+
+                    <p className="step-description">
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Including
+                      versions of Lorem Ipsum.
+                    </p>
+
+                    <div className="panels-navigation text-right">
+                      <div className="nav-panel" data-sign="plus">
+                        <a href="#panel-2" className="anchor">Next</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </article>
+          ))
+        }
+          </div>
+
+        </section>
+      </main>
     </div>
 
   );
