3f893e1825ffca8711adfc80ebbc93a579057a31
Federico <federico.izzop@gmail.com>
2021-03-11 14:56:35 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,35 @@ import '../scss/style.scss';
 
 export default function About() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <p className="display-4">Nel gennaio 2013 Joss Whedon affermò</p>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <div className="row">
+              tasto
+            </div>
+            <div className="row">
+              tasto
+            </div>
+            <div className="row">
+              tasto
+            </div>
+          </div>
+          <div className="col-10 overflow-auto" style={{ height: '90vh' }}>
+            Nel gennaio 2013 Joss Whedon affermò di non vedere un'influenza diretta da altre serie, come X-Files, e spiegò che anche se lo show mostra persone con poteri e delle storie di fantascienza, si concentra sulle persone ai margini di queste grandi avventure".[19] Parlando del bilanciamento tra materiale nuovo e materiale preso dai fumetti, Bell affermò che "la sfida è sin dall'inizio quella di raccontare storie che piacciano sia ai fan della Marvel che al pubblico generalista. Ci sono alcuni modi per farlo. Se possiamo inserire un collegamento ai film, che siano i Chitauri o il cameo di Samuel L. Jackson o le conseguenze della battaglia di New York, quello è divertente, e cerchiamo di farlo in un modo che tutti riescano a seguire la storia. Poi proviamo anche a scavare nell'universo Marvel. Se abbiamo bisogno di un dottore, ci diciamo 'se c'è un dottore nell'universo Marvel, non sarebbe divertente per i fan sentire quel nome? Possiamo portare quel personaggio e le sue qualità?' [...] Per cui cerchiamo di fare in questo modo".[20] Su quanto la serie possa prendere dai fumetti, Jed Whedon spiegò: "Ci sono delle zone da cui possiamo pescare e altre cose che non possiamo usare... ci serve più come ispirazione per pensare alla storia". Tancharoen aggiunse che "Prenderemo sempre ispirazione dai fumetti, ma ovviamente ci aggiungiamo del nostro per lo show".[21]
+            Nel gennaio 2013 Joss Whedon affermò di non vedere un'influenza diretta da altre serie, come X-Files, e spiegò che anche se lo show mostra persone con poteri e delle storie di fantascienza, si concentra sulle persone ai margini di queste grandi avventure".[19] Parlando del bilanciamento tra materiale nuovo e materiale preso dai fumetti, Bell affermò che "la sfida è sin dall'inizio quella di raccontare storie che piacciano sia ai fan della Marvel che al pubblico generalista. Ci sono alcuni modi per farlo. Se possiamo inserire un collegamento ai film, che siano i Chitauri o il cameo di Samuel L. Jackson o le conseguenze della battaglia di New York, quello è divertente, e cerchiamo di farlo in un modo che tutti riescano a seguire la storia. Poi proviamo anche a scavare nell'universo Marvel. Se abbiamo bisogno di un dottore, ci diciamo 'se c'è un dottore nell'universo Marvel, non sarebbe divertente per i fan sentire quel nome? Possiamo portare quel personaggio e le sue qualità?' [...] Per cui cerchiamo di fare in questo modo".[20] Su quanto la serie possa prendere dai fumetti, Jed Whedon spiegò: "Ci sono delle zone da cui possiamo pescare e altre cose che non possiamo usare... ci serve più come ispirazione per pensare alla storia". Tancharoen aggiunse che "Prenderemo sempre ispirazione dai fumetti, ma ovviamente ci aggiungiamo del nostro per lo show".[21]
+
+            Parlando della differenza di budget tra la serie e i film, Bell affermò: "Sia la ABC che la Marvel sono molto generose con il nostro budget. Per essere uno show su un canale generalista non posso lamentarmi... e tuttavia una serie come Game of Thrones ha più del doppio del nostro budget, e dobbiamo competere con show pieni di effetti speciali... il pubblico vuole questi momenti da film Marvel. Vuole l'arma potente, il display olografico. Allo stesso tempo la televisione vive e respira attraverso i personaggi... il nostro scopo è farvi affezionare a loro. Avremo sempre le cose belle della Marvel, e avremo sempre grandi momenti finché il budget ce lo permette, ma equivalgono a 10 minuti di Iron Man. Quello che il pubblico ricorda di Avengers sono i personaggi. C'è quell'enorme scena d'azione in cui i Chitauri invadono New York, ma quando chiedi alle persone qual è il loro momento preferito del film, ti rispondono 'Quando Banner e Stark parlano nel laboratorio', o 'Quando Hulk usa Loki come una bambola di pezza. Quello era divertente.' Sono sempre i personaggi che arrivano alle persone, ed è una cosa che la TV fa molto bene".[20] Jed Whedon aggiunse: "Come possiamo raccontare al meglio la storia, come possiamo cambiare queste sequenze d'azione in modo che siano dal punto di vista dei personaggi? È una cosa di cui parliamo molto. Se un mostro salta su una macchina, invece di far vedere il mostro che salta in mezzo alla città, vogliamo essere nella macchina insieme alla persona che sta vivendo quel momento. Funziona per lo show ma anche sul piano del budget e del tempo a nostra disposizione. Ci aiuta a evitare di usare questi grossi effetti rendendo la scena più personale e viscerale".[22]
+
+            Parlando della differenza di budget tra la serie e i film, Bell affermò: "Sia la ABC che la Marvel sono molto generose con il nostro budget. Per essere uno show su un canale generalista non posso lamentarmi... e tuttavia una serie come Game of Thrones ha più del doppio del nostro budget, e dobbiamo competere con show pieni di effetti speciali... il pubblico vuole questi momenti da film Marvel. Vuole l'arma potente, il display olografico. Allo stesso tempo la televisione vive e respira attraverso i personaggi... il nostro scopo è farvi affezionare a loro. Avremo sempre le cose belle della Marvel, e avremo sempre grandi momenti finché il budget ce lo permette, ma equivalgono a 10 minuti di Iron Man. Quello che il pubblico ricorda di Avengers sono i personaggi. C'è quell'enorme scena d'azione in cui i Chitauri invadono New York, ma quando chiedi alle persone qual è il loro momento preferito del film, ti rispondono 'Quando Banner e Stark parlano nel laboratorio', o 'Quando Hulk usa Loki come una bambola di pezza. Quello era divertente.' Sono sempre i personaggi che arrivano alle persone, ed è una cosa che la TV fa molto bene".[20] Jed Whedon aggiunse: "Come possiamo raccontare al meglio la storia, come possiamo cambiare queste sequenze d'azione in modo che siano dal punto di vista dei personaggi? È una cosa di cui parliamo molto. Se un mostro salta su una macchina, invece di far vedere il mostro che salta in mezzo alla città, vogliamo essere nella macchina insieme alla persona che sta vivendo quel momento. Funziona per lo show ma anche sul piano del budget e del tempo a nostra disposizione. Ci aiuta a evitare di usare questi grossi effetti rendendo la scena più personale e viscerale".[22]
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 }
