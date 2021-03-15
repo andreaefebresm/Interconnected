03fa0d278d58be80cc63bfde75e613a636ab34c4
Federico <federico.izzop@gmail.com>
2021-03-15 22:37:38 +0100

    tenttivi

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-primary">
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <Link className="navbar-brand messapiaReg " to="/menu">InterConnected</Link>
--- src/pages/firstOption.js
+++ src/pages/firstOption.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Header from '../components/Header';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import '../scss/style.scss';
 import { Link } from 'gatsby';
+import { gsap } from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function UseSafely() {
-  return (
+  const panels1 = useRef();
 
-    <div>
-      <Header />
+  useEffect(() => {
+    gsap.utils.toArray('.panel1').forEach((panels1, i) => {
+      ScrollTrigger.create({
+        trigger: panels1,
+        start: 'top top',
+        pin: true,
+        pinSpacing: false,
+      });
+    });
+
+    ScrollTrigger.create({
+      snap: 1 / 4, // snap whole page to the closest section!
+    });
+  });
+
+  return (
+    <div ref={panels1}>
+
+      <section className="panel1 red full-screen bg-primary">
+        ONE
+      </section>
+      <section className="panel1 full-screen bg-secondary">
+        TWO
+      </section>
+      <section className="panel1 full-screen bg-dark">
+        THREE
+      </section>
+      <section className="panel1 full-screen bg-light">
+        THREE
+      </section>
+      <section className="panel1 full-screen bg-success">
+        FOUR
+      </section>
 
     </div>
   );
