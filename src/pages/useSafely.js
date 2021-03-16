6d9caf6dda3e004dc17794f3d63b43e436f74d96
Federico <federico.izzop@gmail.com>
2021-03-16 12:34:52 +0100

    svg start

--- src/pages/useSafely.js
+++ src/pages/useSafely.js
@@ -6,7 +6,59 @@ import { Link } from 'gatsby';
 import { gsap } from 'gsap';
 
 export default function UseSafely() {
+  const article = [
+    {
+      who: 'aa',
+      title: 'ee',
+      decription: 'questa è una descrizione dellarticolo',
+      link: '',
+    },
+    {
+      who: 'aa',
+      title: 'ee',
+      decription: 'questa è una descrizione dellarticolo',
+      link: '',
+    },
+    {
+      who: 'aa',
+      title: 'ee',
+      decription: 'questa è una descrizione dellarticolo',
+      link: '',
+    },
+    {
+      who: 'aa',
+      title: 'ee',
+      decription: 'questa è una descrizione dellarticolo',
+      link: '',
+    },
+
+  ];
   return (
-    <Header />
+    <div>
+      <Header />
+      <section>
+        <p>What can you do</p>
+        <p>This archive collects guides to opt out from personalisation and to use more safely your devices. </p>
+      </section>
+      <section>
+        <div className="container-fluid">
+          <div className="row">
+            {
+              article.map(({
+                who, title, decription,
+              }) => (
+                <div className="col-3">
+                  <p>{who}</p>
+                  <p>{title}</p>
+                  <p>{decription}</p>
+                </div>
+              ))
+            }
+
+          </div>
+        </div>
+      </section>
+    </div>
+
   );
 }
