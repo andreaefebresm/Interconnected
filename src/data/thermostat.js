e8bc98dc3e810ef8a83d62aab3eb6451430564a3
Federico <federico.izzop@gmail.com>
2021-03-24 11:14:00 +0100

    data taken thermostat

--- src/data/thermostat.js
+++ src/data/thermostat.js
@@ -6,6 +6,13 @@ import thermostat1 from '../images/thermostat/thermostat1.jpg';
 import thermostat2 from '../images/thermostat/thermostat2.jpg';
 import thermostat3 from '../images/thermostat/thermostat3.jpg';
 
+const presence = { title: 'Presence in the house', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+const rooms = { title: 'Rooms usage', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+const tone = { title: 'Tone of voice', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+const voice = { title: 'Voice recognition', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+const query = { title: 'Query history', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+const dist = { title: 'Distance from the device', descr: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the.' };
+
 const thermostat = {
   title: 'SMART THERMOSTAT',
   description: 'The thermostats can be controlled remotly and they regulate the temperature by themselves.',
@@ -19,19 +26,19 @@ const thermostat = {
 
       collectedData: [
         {
-          label: 'Map of the house',
-          description: 'PROXIMITY SENSORS COMBINED WITH ARTIFICIAL INTELLINGENCE* RECOGNISE OBSTACLES AS WALLS.\n'
-            + 'USERS GIVE THE NAME OF EACH ROOM.',
+          label: presence.title,
+          description: presence.descr,
+          style: {
+            left: '10%',
+          },
         },
         {
-          label: 'Map of the house',
-          description: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the '
-            + 'company’s servers and an A.I. recognises if the object is a table. The photos are stored only for few seconds '
-            + 'but the fact that you own such type of furniture remains. ',
-        },
-        {
-          label: 'Map of the house',
-          description: 'The vacuum robot analyses how many times it has to empty the bin and in which room the bin gets fuller.',
+          label: rooms.title,
+          description: rooms.descr,
+          style: {
+            top: '30%',
+            left: '21%',
+          },
         },
       ],
     },
@@ -42,19 +49,19 @@ const thermostat = {
 
       collectedData: [
         {
-          label: 'Map of the house',
-          description: 'PROXIMITY SENSORS COMBINED WITH ARTIFICIAL INTELLINGENCE* RECOGNISE OBSTACLES AS WALLS.\n'
-            + 'USERS GIVE THE NAME OF EACH ROOM.',
+          label: presence.title,
+          description: presence.descr,
+          style: {
+            left: '10%',
+          },
         },
         {
-          label: 'Map of the house',
-          description: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the '
-            + 'company’s servers and an A.I. recognises if the object is a table. The photos are stored only for few seconds '
-            + 'but the fact that you own such type of furniture remains. ',
-        },
-        {
-          label: 'Map of the house',
-          description: 'The vacuum robot analyses how many times it has to empty the bin and in which room the bin gets fuller.',
+          label: rooms.title,
+          description: rooms.descr,
+          style: {
+            top: '30%',
+            left: '21%',
+          },
         },
       ],
     },
@@ -65,19 +72,50 @@ const thermostat = {
 
       collectedData: [
         {
-          label: 'Map of the house',
-          description: 'PROXIMITY SENSORS COMBINED WITH ARTIFICIAL INTELLINGENCE* RECOGNISE OBSTACLES AS WALLS.\n'
-            + 'USERS GIVE THE NAME OF EACH ROOM.',
+          label: presence.title,
+          description: presence.descr,
+          style: {
+            left: '10%',
+          },
         },
         {
-          label: 'Map of the house',
-          description: 'Some smart vacuum cleaners have cameras in them and take photos. These photos are sent to the '
-            + 'company’s servers and an A.I. recognises if the object is a table. The photos are stored only for few seconds '
-            + 'but the fact that you own such type of furniture remains. ',
+          label: rooms.title,
+          description: rooms.descr,
+          style: {
+            top: '30%',
+            left: '21%',
+          },
         },
         {
-          label: 'Map of the house',
-          description: 'The vacuum robot analyses how many times it has to empty the bin and in which room the bin gets fuller.',
+          label: tone.title,
+          description: tone.descr,
+          style: {
+            left: '10%',
+          },
+        },
+        {
+          label: dist.title,
+          description: dist.descr,
+          style: {
+            top: '50%',
+            left: '23%',
+          },
+        },
+        {
+          label: query.title,
+          description: query.descr,
+          style: {
+            left: '5%',
+            top: '70%',
+          },
+        },
+        {
+          label: voice.title,
+          description: voice.descr,
+          style: {
+            left: '22%',
+            top: '70%',
+          },
         },
       ],
     },
