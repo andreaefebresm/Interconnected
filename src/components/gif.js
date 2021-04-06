d65d94ca2f4ed97b3a1b0ee246c5cd6d8b4e5b35
Federico <federico.izzop@gmail.com>
2021-04-06 11:51:51 +0200

    fix gif and primary

--- src/components/gif.js
+++ src/components/gif.js
@@ -5,7 +5,7 @@ export default function Gif() {
   return (
     <div className="d-flex justify-content-center">
 
-      <img src={introGif} style={{ transform: 'scale(1)' }} />
+      <img src={introGif} style={{ transform: 'scale(0.9)' }} />
     </div>
   );
 }
