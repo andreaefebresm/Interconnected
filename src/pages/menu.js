dde2d63b13a169e95678b4714fb63013c5359d88
michelebruno <43304601+michelebruno@users.noreply.github.com>
2021-04-26 11:07:51 +0200

    Responsive menu and svg font fixed

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -10,8 +10,10 @@ export default function Menu() {
     <Layout title="Menu">
       <div className="container-fluid pt-5">
         <div className="row">
-          <h1 className="offset-1 col-9 display-3 bigText">Do you know what smart objects can tell about you?</h1>
-          <h3 className="offset-1 display-5 fontTextItalic">Choose one device to discover it.</h3>
+          <div className="col">
+            <h1 className="offset-1 col-9 display-3 bigText">Do you know what smart objects can tell about you?</h1>
+            <h3 className="offset-1 display-5 fontTextItalic">Choose one device to discover it.</h3>
+          </div>
         </div>
 
         <div className="row  justify-content-center" style={{ paddingTop: '5vh' }}>
@@ -20,19 +22,27 @@ export default function Menu() {
               title, description, items, Icon,
             }, index) => (
               <div
-                className="col-12 col-md-3 col-lg-2 mb-5 card scuro"
+                className="col-12 col-md-3 col-lg-2 mx-lg-3 mb-3 mb-md-5"
                 key={title}
-                onClick={() => navigate(`/start?selected=${index}`)}
-                role="link"
                 style={{
-                  cursor: 'pointer', marginRight: '20px', marginLeft: '20px',
+                  cursor: 'pointer',
                 }}
               >
-                <div className="card-body">
-                  <Icon style={{ height: '15vh', paddingTop: '15px', paddingBottom: '15px' }} className="mx-auto w-100" />
-                  <h2 className="display-7 fontTextStrong pt-2 ">{title}</h2>
-                  <p className="display-10 fontText">{description}</p>
-                  <p className="display-6 fontTextItalic">{items}</p>
+                <div
+                  className="card h-100 card scuro "
+                  onClick={() => navigate(`/start?selected=${index}`)}
+                  role="link"
+                >
+
+                  <div className="card-body">
+                    <Icon
+                      style={{ height: '15vh', paddingTop: '15px', paddingBottom: '15px' }}
+                      className="mx-auto w-100"
+                    />
+                    <h2 className="display-7 fontTextStrong pt-2 ">{title}</h2>
+                    <p className="display-10 fontText">{description}</p>
+                    <p className="display-6 fontTextItalic">{items}</p>
+                  </div>
                 </div>
               </div>
 
